2331367bbb34f2d727e0e7ef091172dcb155e57a
Matthijs Krul <matthijs.a.krul@gmail.com>
2019-10-22 00:48:38 +0200

    Changed logo for conlang doc index

--- RST/conf.py
+++ RST/conf.py
@@ -50,6 +50,8 @@ html_theme_options = {
     'wending_last_updated' : False
 }
 
+html_logo = '_static/owl.png'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
